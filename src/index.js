43f91c09296b7b1ef2b835a4247eb2e4e1a237b8
Robin de Mourat <robin.demourat@gmail.com>
2019-12-09 08:38:57 +0100

    add a loader

--- src/index.js
+++ src/index.js
@@ -226,6 +226,76 @@ function generateOutput ( {
         </div>
 
       ${allowAnnotation ? '<script src="https://hypothes.is/embed.js" async></script>' : ''}
+        <style>
+#static-loader-container{
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: rgba(0,0,0,0.1);
+  opacity: 0;
+  transition: .5s ease;
+}
+.lds-ellipsis {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-ellipsis div {
+  position: absolute;
+  top: 33px;
+  width: 13px;
+  height: 13px;
+  border-radius: 50%;
+  background: black;
+  animation-timing-function: cubic-bezier(0, 1, 1, 0);
+}
+.lds-ellipsis div:nth-child(1) {
+  left: 8px;
+  animation: lds-ellipsis1 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(2) {
+  left: 8px;
+  animation: lds-ellipsis2 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(3) {
+  left: 32px;
+  animation: lds-ellipsis2 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(4) {
+  left: 56px;
+  animation: lds-ellipsis3 0.6s infinite;
+}
+@keyframes lds-ellipsis1 {
+  0% {
+    transform: scale(0);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+@keyframes lds-ellipsis3 {
+  0% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(0);
+  }
+}
+@keyframes lds-ellipsis2 {
+  0% {
+    transform: translate(0, 0);
+  }
+  100% {
+    transform: translate(24px, 0);
+  }
+}
+        </style>
         <script>
               function loadJS(url, location){
                   //url is URL of external file, implementationCode is the code
@@ -247,7 +317,23 @@ function generateOutput ( {
                 };
                 xobj.send(null);  
             }
-            var urlPrefix = window.location.origin + '/'
+            function addLoader() {
+              var loader = document.createElement('div')
+              loader.id = 'static-loader-container';
+              loader.innerHTML = '<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>';
+              document.body.appendChild(loader);
+              loader.style.opacity = 1;
+            }
+            function hideLoader() {
+              var loader = document.getElementById('static-loader-container')
+              loader.style.opacity = 0;
+              loader.style.pointerEvents = 'none';
+            }
+            var urlPrefix = window.location.origin + '/';
+            /**
+             * add loader
+             */
+
             /**
              * Dynamically loading the JSON data
              */
@@ -264,6 +350,7 @@ function generateOutput ( {
                  */
                 var bundleURL = urlPrefix + 'bundle.js';
                 loadJS(bundleURL, document.body);
+                setTimeout(() => hideLoader());
               });
 
               `
@@ -288,6 +375,7 @@ function generateOutput ( {
               .map( ( key ) => `var ${key} = "${config[key]}"` )
               .join( '\n' )
             }
+            
         </script>
         <script type="text/javascript" src="/bundle.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/css-vars-ponyfill@1"></script>
